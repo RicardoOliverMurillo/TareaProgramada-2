23814bedb37567134a63725e282aedc36bdbdf36
anjelicat98 <anjelica.tristani98@gmail.com>
2018-10-07 17:55:57 -0600

    Crear el histograma a partir de la variable seleccionada

--- pruebaCommit.R
+++ pruebaCommit.R
@@ -1,3 +1,34 @@
-#Test commit
-#test commit vegan 
-#ESTOY HACIENDO UN COMMIT POR RICHARDDDDD
+library(shiny)
+library(ggplot2)
+
+#Lectura de datos
+datos <- read.csv("datosProyecto2.csv", header = TRUE, stringsAsFactors = FALSE, sep = ';')
+#Lectura de datos sin valores NA
+datosCompletos <- na.omit(datos)
+
+#Parte de interfaz gráfica
+ui <- fluidPage(
+  
+  #titulo principal de la página
+  title = "Black Friday Dataset Analysis",
+  headerPanel("Black Friday Dataset Analysis"),
+  
+  "5. Histograma",
+ 
+   #crea el comboBox para seleccionar la variable
+  selectInput(inputId =  "var", label = "Seleccionar variable para generar histograma:", 
+              colnames(datosCompletos[3:8])),
+  plotOutput('hist')
+)
+
+server <- function(input, output){
+  
+  #Genera el gráfico a partir del output del id var
+  output$hist <- renderPlot({
+    ggplot(data =datosCompletos, aes_string(x = input$var)) +geom_bar(width = 0.5)+ ggtitle("Histograma generado a partir de variable")+ xlab("Variable seleccionada")
+  })
+  
+}
+
+#Junta el servidor con la gui
+shinyApp(ui = ui, server = server)
