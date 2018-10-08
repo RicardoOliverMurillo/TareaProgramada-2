7215796c6bb5f5e8ea4f342dc6c72828c7e4ea12
anjelicat98 <43051211+anjelicat98@users.noreply.github.com>
2018-10-07 23:05:21 -0600

    Update pruebaCommit.R

--- pruebaCommit.R
+++ pruebaCommit.R
@@ -6,6 +6,7 @@ datos <- read.csv("datosProyecto2.csv", header = TRUE, stringsAsFactors = FALSE,
 #Lectura de datos sin valores NA
 datosCompletos <- na.omit(datos)
 
+
 #Parte de interfaz gráfica
 ui <- fluidPage(
   
@@ -13,18 +14,23 @@ ui <- fluidPage(
   title = "Black Friday Dataset Analysis",
   headerPanel("Black Friday Dataset Analysis"),
   
-  "2. Summary",
+  h3("1. Summary"),
   #crea el comboBox para calcular la estadística
-  selectInput(inputId =  "sum", label = "Seleccionar variable para generar estadísticas:", 
+  selectInput(inputId =  "sum", label = "Seleccione una variable para generar las estadísticas:", 
               colnames(datos[3:8])),
   #conecta con el servidor
   verbatimTextOutput('summary'),
   
+  h3("3. Mostrar N registros completos"),
+  #textInput(inputId = "Nregistros", label = "Ingrese la cantidad de registros que desea ver"),
+  textInput("Nregistros", "Ingrese la cantidad de registros que desea ver:"),
+  actionButton(inputId = "N", "Mostrar registros"),
+  dataTableOutput("tablaN"),
   
-  "5. Histograma",
+  h3("4. Histograma"),
  
    #crea el comboBox para seleccionar la variable
-  selectInput(inputId =  "var", label = "Seleccionar variable para generar histograma:", 
+  selectInput(inputId =  "var", label = "Seleccione una variable para generar histograma:", 
               colnames(datosCompletos[3:8])),
   #conecta con el servidor
   plotOutput('hist')
@@ -35,6 +41,27 @@ server <- function(input, output){
   #muestra los datos estadísticos de las variables
   output$summary <- renderPrint({
     summary(datosCompletos[input$sum])
+  })
+  
+  #funcion que verifica qye la entrada sea mayor a 1 y muestra los registros del número ingresado
+  mostrarRegistro <- eventReactive(input$N,{
+    x <- as.integer(input$Nregistros)
+    print(str(x))
+    if(x>=1){
+      datosCompletos[1:x,] #selecciona las filas
+    }else{ #mensaje de error si el número es menor a 1
+      showModal(modalDialog(
+        title = "ERROR",
+        "Valor debe ser mayor a 1",
+        easyClose = TRUE,
+        footer = NULL
+      ))
+    }
+  })
+  
+  #muestra la tabla con los N datos
+  output$tablaN <- renderDataTable({
+    mostrarRegistro()
   })
   
   #Genera el gráfico a partir del output del id var
