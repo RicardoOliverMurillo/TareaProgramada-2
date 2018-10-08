0672d6e13b68810a81d4edb8ec806eebcd90736f
anjelicat98 <43051211+anjelicat98@users.noreply.github.com>
2018-10-07 21:48:44 -0600

    Update pruebaCommit.R

--- pruebaCommit.R
+++ pruebaCommit.R
@@ -13,20 +13,34 @@ ui <- fluidPage(
   title = "Black Friday Dataset Analysis",
   headerPanel("Black Friday Dataset Analysis"),
   
+  "2. Summary",
+  #crea el comboBox para calcular la estadística
+  selectInput(inputId =  "sum", label = "Seleccionar variable para generar estadísticas:", 
+              colnames(datos[3:8])),
+  #conecta con el servidor
+  verbatimTextOutput('summary'),
+  
+  
   "5. Histograma",
  
    #crea el comboBox para seleccionar la variable
   selectInput(inputId =  "var", label = "Seleccionar variable para generar histograma:", 
               colnames(datosCompletos[3:8])),
+  #conecta con el servidor
   plotOutput('hist')
 )
 
 server <- function(input, output){
   
+  #muestra los datos estadísticos de las variables
+  output$summary <- renderPrint({
+    summary(datosCompletos[input$sum])
+  })
+  
   #Genera el gráfico a partir del output del id var
   output$hist <- renderPlot({
-    ggplot(data =datosCompletos, aes_string(x = input$var)) +geom_bar(width = 0.5)+ ggtitle("Histograma generado a partir de variable")+ 
-      xlab(input$var)
+    ggplot(data =datosCompletos, aes_string(x = input$var)) +geom_bar()+ ggtitle("Histograma generado a partir de variable")+ 
+      xlab(input$var)+theme(panel.background = element_rect(fill = "pink"))
   })
   
 }
